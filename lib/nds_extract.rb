0d4245dc5fc2ce23e7fb95ca9d320d735d2f455e
Vivica Ma <vivicacongma@gmail.com>
2020-02-21 06:39:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,5 +54,18 @@ def total_gross(source)
 #   total
 # end
 
+dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
 
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
+  end
+
+  total
+end
 
